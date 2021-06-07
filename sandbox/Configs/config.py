27a2da63de5d2ccbe7b007c4b4800f84a515932a
Chann <34920304+devChann@users.noreply.github.com>
2021-06-07 22:16:43 +0300

    intial upload
    initial commit

---
+++ sandbox/Configs/config.py
@@ -0,0 +1,23 @@
+
+Baseline = {"Canola": {"prep": [400, 480],
+                       "temp": [6, 10, 26], "period": "May",
+                       "humid": "null", "soilM": [80, 120], "ph": [5.5, 8.5]},
+            "Peas": {"prep": [300, 370], "temp": [6], "period": "April", "humid": "null", "soilM": [175, 250], "ph": [6, 7.5]},
+            "Wheat": {"prep": [275, 325], "temp": [5, 6, 10, 26], "period": "May", "humid": "null", "soilM": [175-250], "ph": [6, 7]},
+            "Lentils": {"prep": [150, 175], "temp": [5, 6, 10, 26], "period": "", "humid": "null", "soilM": [175, 250], "ph": [6, 6.5]},
+            "Flax": {"prep": [340, 440], "temp": [3, 4, 10, 26], "period": "May", "humid": "null", "soilM": [175, 250], "ph": [5, 5.5]},
+            "SoyBeans": {"prep": [300, 600], "temp": [10, 12, 10, 26], "period": "May", "humid": "null", "soilM": [175, 250], "ph": [6.3, 6.5]},
+            "Fabas_Beans": {"prep": [300, 380], "temp": [5], "period": "May", "humid": "null", "soilM": [175, 250], "ph": [5.5, 9]}, "Hemp": {"prep": [300, 380],                                                                                                                                                                                                                                                                                                  "temp": [4, 6, 8, 10], "period": "May", "humid": "null", "soilM": [175-250], "ph": [5.5, 6.5]},
+            "Potatoes": {"prep": [400, 550], "temp": [5, 7, 18], "period": "May",
+                         "humid": "null", "soilM": [175, 250], "ph": [5, 7.5]},
+            "Barley": {"prep": [400, 550], "temp": [5, 7, 18], "period": "May",
+                       "humid": "null", "soilM": [175, 250], "ph": [5, 7.5]},
+            "Corn": {"prep": [380, 440], "temp": [5, 10, 10, 26], "period": "May", "humid": "null", "soilM": [175, 250], "ph": [6, 8.5]},
+            "Sunflower": {"prep": [580, 650], "temp": [12], "period": "April", "humid": "null", "soilM": [175250], "ph": [5.8, 6.2]},
+            "Durum": {"prep": [275, 325], "temp": [10, 30, 10-12, 25, 28], "period": "April",
+                      "humid": "null", "soilM": [175, 250], "ph": [6, 7.5]},
+            "Fall_raye": {"prep": [275, 325], "temp": [5, 6, 10, 26], "period": "May", "humid": "null", "soilM": [28, 250], "ph": [6, 7]},
+            "Winter_wheat": {"prep": [], "temp": [], "period": "May", "humid": "null", "soilM": [175, 250], "ph": [6.5, 7]}}
+Database = {
+
+}
