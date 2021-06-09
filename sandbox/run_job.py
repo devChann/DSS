a6edd7c3c7776ba3e6f2db13b34fbc01e1e46764
Chann <princechanns@gmail.com>
2021-06-09 13:43:18 +0300

    change timer

--- sandbox/run_job.py
+++ sandbox/run_job.py
@@ -2,7 +2,7 @@ from .DSS_system import execute_job
 import schedule
 import time
 
-schedule.every().day.at("09:00").do(execute_job)
+schedule.every().day.at("13:48").do(execute_job)
 
 while True:
     schedule.run_pending()
