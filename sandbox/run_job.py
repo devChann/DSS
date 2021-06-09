7d41a4b5fbc4666b98b81c4f859d626930fc351d
Chann <princechanns@gmail.com>
2021-06-09 13:52:02 +0300

    sys structure

--- sandbox/run_job.py
+++ sandbox/run_job.py
@@ -1,8 +1,8 @@
-from .DSS_system import execute_job
+from DSS import execute_job
 import schedule
 import time
 
-schedule.every().day.at("13:48").do(execute_job)
+schedule.every().day.at("13:55").do(execute_job)
 
 while True:
     schedule.run_pending()
