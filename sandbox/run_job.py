15ae978e7616f00cc5fd04b0cb08c0ce7e126163
Chann <princechanns@gmail.com>
2021-06-11 18:39:26 +0300

    tests

--- sandbox/run_job.py
+++ sandbox/run_job.py
@@ -2,8 +2,11 @@ from DSS import execute_job
 import schedule
 import time
 
-schedule.every().day.at("18:36").do(execute_job)
+schedule.every().day.at("18:40").do(execute_job)
 
 while True:
     schedule.run_pending()
     time.sleep(300)
+
+# testing
+execute_job()
